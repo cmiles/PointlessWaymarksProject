5b13e0323122e7a4531c2a982ab096851fbd0dea
Charles Miles <charles@cmiles.info>
2021-12-04 20:57:10 -0700

    Change Leaflet to add more Zoom Levels in OpenTopo even if not 'truly' present.

--- PointlessWaymarks.CmsData/SiteResources/pointless-waymarks-spatial-common.js
+++ PointlessWaymarks.CmsData/SiteResources/pointless-waymarks-spatial-common.js
@@ -11,7 +11,8 @@
 function openTopoMapLayer() {
     return L.tileLayer("https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png",
         {
-            maxZoom: 17,
+            maxNativeZoom: 17,
+            maxZoom: 22,
             id: "osmTopo",
             attribution:
                 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
