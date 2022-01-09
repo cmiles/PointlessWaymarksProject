70a7417d5f8d1ed9aff19468cbcf6219babadbeb
Charles Miles <charles@cmiles.info>
2022-01-08 22:47:47 -0700

    Fix Drag and Drop Bug in BodyContentEditorHorizontal

--- PointlessWaymarks.CmsWpfControls/BodyContentEditor/BodyContentEditorHorizontalControl.xaml.cs
+++ PointlessWaymarks.CmsWpfControls/BodyContentEditor/BodyContentEditorHorizontalControl.xaml.cs
@@ -1,4 +1,9 @@
-﻿namespace PointlessWaymarks.CmsWpfControls.BodyContentEditor;
+﻿using System.Diagnostics;
+using System.Windows;
+using System.Windows.Controls;
+using PointlessWaymarks.WpfCommon.Utility;
+
+namespace PointlessWaymarks.CmsWpfControls.BodyContentEditor;
 
 /// <summary>
 ///     Interaction logic for BodyContentEditorHorizontalControl.xaml
@@ -8,5 +13,30 @@ public partial class BodyContentEditorHorizontalControl
     public BodyContentEditorHorizontalControl()
     {
         InitializeComponent();
+    }
+
+    private void BodyContentTextBox_OnSelectionChanged(object sender, RoutedEventArgs e)
+    {
+        if (DataContext is not BodyContentEditorContext context) return;
+        if (sender is not TextBox t) return;
+
+        context.UserBodyContentUserSelectionStart = t.SelectionStart;
+        context.UserBodyContentUserSelectionLength = t.SelectionLength;
+    }
+
+    private void TextBoxPreviewDragOver(object sender, DragEventArgs e)
+    {
+        if (sender is not TextBox textBox || e == null) return;
+
+        // Set the caret at the position where user ended the drag-drop operation
+        var dropPosition = e.GetPosition(textBox);
+        textBox.SelectionStart = TextBoxHelpers.GetCaretIndexFromPoint(textBox, dropPosition);
+        textBox.SelectionLength = 0;
+
+        Debug.WriteLine($"{textBox.SelectionStart}");
+
+        // don't forget to set focus to the text box to make the caret visible!
+        textBox.Focus();
+        e.Handled = true;
     }
 }
