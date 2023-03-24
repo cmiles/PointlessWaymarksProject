191c64ee3241038dd983cd58aec1c182eb005528
Charles Miles <charles@cmiles.info>
2023-03-24 09:15:17 -0700

    WpfCommon: Multi Converter for Visibility and a list of bools

---
+++ PointlessWaymarks.WpfCommon/ValueConverters/VisibleWhenAllTrueOtherwiseCollapsedMultiConverter.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace PointlessWaymarks.WpfCommon.ValueConverters;
+
+public class VisibleWhenAllTrueOtherwiseCollapsedMultiConverter : IMultiValueConverter
+{
+    public object? Convert(object[]? values, Type? targetType, object? parameter, CultureInfo culture)
+    {
+        if (values == null) return Visibility.Collapsed;
+        if (values.Length == 0) return Visibility.Collapsed;
+
+        foreach (var valueLoop in values)
+        {
+            if (valueLoop is true) continue;
+            return Visibility.Collapsed;
+        }
+
+        return Visibility.Visible;
+    }
+
+    public object[]? ConvertBack(object? value, Type[]? targetTypes, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
