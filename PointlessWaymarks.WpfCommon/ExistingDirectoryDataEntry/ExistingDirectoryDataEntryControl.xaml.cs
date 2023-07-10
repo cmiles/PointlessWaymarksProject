0ce02f6409269f1f2d6d434262d20d71ab328cde
Charles Miles <charles@cmiles.info>
2023-07-10 10:14:10 -0700

    WpfCommon: Add a new Existing Directory Picker Control

---
+++ PointlessWaymarks.WpfCommon/ExistingDirectoryDataEntry/ExistingDirectoryDataEntryControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace PointlessWaymarks.WpfCommon.ExistingDirectoryDataEntry
+{
+    /// <summary>
+    /// Interaction logic for DirectoryDataEntryControl.xaml
+    /// </summary>
+    public partial class ExistingDirectoryDataEntryControl : UserControl
+    {
+        public ExistingDirectoryDataEntryControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
