11af56c6b2e0b847351884d4e4eade3ba8187a7b
Charles Miles <charles@cmiles.info>
2023-07-23 11:35:29 -0700

    Cloud Backup: List Item Clear Progress Timer
    The single line progress shown in the Job List Items was confusing after a job ends because it would stay on the last progress entry 'forever'. Added a time to clear after 4 minutes - that will mean it could be cleared during long uploads but I think given the intent of this feature it is a decent compromise...

--- PointlessWaymarks.CloudBackupEditorGui/Controls/JobListListItem.cs
+++ PointlessWaymarks.CloudBackupEditorGui/Controls/JobListListItem.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel;
+using System.Timers;
 using PointlessWaymarks.CloudBackupData.Models;
 using PointlessWaymarks.LlamaAspects;
 
@@ -6,10 +8,32 @@ namespace PointlessWaymarks.CloudBackupEditorGui.Controls;
 [NotifyPropertyChanged]
 public partial class JobListListItem
 {
+    private readonly System.Timers.Timer _progressTimer = new(240000);
+
     public JobListListItem(BackupJob job)
     {
         DbJob = job;
         PersistentId = job.PersistentId;
+
+        PropertyChanged += OnPropertyChanged;
+        _progressTimer.Elapsed += RemoveProgress;
+    }
+
+    private void RemoveProgress(object? sender, ElapsedEventArgs e)
+    {
+            ProgressString = string.Empty;
+            _progressTimer.Stop();
+    }
+
+
+    private void OnPropertyChanged(object? sender, PropertyChangedEventArgs e)
+    {
+        if(e.PropertyName == nameof(ProgressString) && !string.IsNullOrWhiteSpace(ProgressString))
+        {
+
+                _progressTimer.Stop();
+                _progressTimer.Start();
+        }
     }
 
     public BackupJob? DbJob { get; set; }
