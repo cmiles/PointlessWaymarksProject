97ff844e69c5e9f666a88acec39e8923a01b771a
Charles Miles <charles@cmiles.info>
2023-07-31 11:05:34 -0700

    Scripts: Add Single Task Publish Scripts

---
+++ Publish-Task-PhotoPickup.ps1
@@ -0,0 +1,26 @@
+$ErrorActionPreference = "Stop"
+
+$PublishVersion = get-date -f yyyy-MM-dd-HH-mm
+
+$GitCommit = & git rev-parse --short HEAD
+
+dotnet clean .\PointlessWaymarks.sln -property:Configuration=Release -property:Platform=x64 -verbosity:minimal
+
+dotnet restore .\PointlessWaymarks.sln -r win10-x64 -verbosity:minimal
+
+$vsWhere = "{0}\Microsoft Visual Studio\Installer\vswhere.exe" -f ${env:ProgramFiles(x86)}
+
+$msBuild = & $vsWhere -latest -prerelease -requires Microsoft.Component.MSBuild -find MSBuild\**\Bin\MSBuild.exe
+
+& $msBuild .\PointlessWaymarks.sln -property:Configuration=Release -property:Platform=x64 -verbosity:minimal
+
+if ($lastexitcode -ne 0) { throw ("Exec: " + $errorMessage) }
+
+$publishPath = "M:\PointlessWaymarksPublications\PointlessWaymarks.Task.PhotoPickup"
+if(!(test-path -PathType container $publishPath)) { New-Item -ItemType Directory -Path $publishPath }
+
+Remove-Item -Path $publishPath\* -Recurse
+
+& $msBuild .\PointlessWaymarks.Task.PhotoPickup\PointlessWaymarks.Task.PhotoPickup.csproj -t:publish -p:PublishProfile=.\PointlessWaymarks.Task.PhotoPickup\Properties\PublishProfile\FolderProfile.pubxml -verbosity:minimal
+
+if ($lastexitcode -ne 0) { throw ("Exec: " + $errorMessage) }
