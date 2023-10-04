bd620efc55f6e96ccd43f7ec24d537b672af8c82
Charles Miles <charles@cmiles.info>
2023-10-03 18:35:44 -0700

    GpxImport: Bug fix for new Garmin auth library

--- PointlessWaymarks.Task.GarminConnectGpxImport/GpxTrackImport.cs
+++ PointlessWaymarks.Task.GarminConnectGpxImport/GpxTrackImport.cs
@@ -1,8 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
 using System.Diagnostics;
 using System.Text.Json;
-using Garmin.Connect;
-using Garmin.Connect.Auth;
 using NetTopologySuite.IO;
 using PointlessWaymarks.CmsData;
 using PointlessWaymarks.CmsData.CommonHtml;
@@ -10,6 +8,7 @@ using PointlessWaymarks.CmsData.Content;
 using PointlessWaymarks.CmsData.Database;
 using PointlessWaymarks.CommonTools;
 using PointlessWaymarks.FeatureIntersectionTags;
+using PointlessWaymarks.GarminConnect;
 using PointlessWaymarks.SpatialTools;
 using Polly;
 using Serilog;
@@ -182,9 +181,7 @@ public class GpxTrackImport
             password = credentials.password;
         }
 
-        var authParameters = new BasicAuthParameters(username, password);
-
-        var client = new GarminConnectClient(new GarminConnectContext(new HttpClient(), authParameters));
+        var client = new GarminConnectClient(new GarminConnectContext(new HttpClient(), username, password));
 
         var fileList = new List<(FileInfo activityFileInfo, FileInfo? gpxFileInfo)>();
 
