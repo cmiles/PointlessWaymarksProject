806f812a2859c76381a07c8b5de1bb71f582eac2
Charles Miles <charles@cmiles.info>
2023-12-12 10:36:13 -0700

    FeedReader: Improve Link Handling sending all in Item Viewer links to External Browser

--- PointlessWaymarks.FeedReaderGui/Controls/FeedItemListControl.xaml.cs
+++ PointlessWaymarks.FeedReaderGui/Controls/FeedItemListControl.xaml.cs
@@ -1,6 +1,5 @@
-﻿using System.Windows;
-using DocumentFormat.OpenXml.Drawing.Charts;
-using Microsoft.Web.WebView2.Core;
+﻿using Microsoft.Web.WebView2.Core;
+using PointlessWaymarks.WpfCommon.Utility;
 
 namespace PointlessWaymarks.FeedReaderGui.Controls;
 
@@ -11,23 +10,38 @@ public partial class FeedItemListControl
         InitializeComponent();
     }
 
-    private void BodyContentWebView_OnCoreWebView2InitializationCompleted(object? sender, CoreWebView2InitializationCompletedEventArgs e)
+    private void BodyContentWebView_OnCoreWebView2InitializationCompleted(object? sender,
+        CoreWebView2InitializationCompletedEventArgs e)
     {
         BodyContentWebView.CoreWebView2.BasicAuthenticationRequested += (o, args) =>
         {
             if (DataContext is not FeedItemListContext context) return;
-            
+
             args.Response.UserName = context.DisplayBasicAuthUsername;
             args.Response.Password = context.DisplayBasicAuthPassword;
         };
     }
-    
-    private void RssContentWebView_OnCoreWebView2InitializationCompleted(object? sender, CoreWebView2InitializationCompletedEventArgs e)
+
+    private void BodyContentWebView_OnNavigationStarting(object? sender, CoreWebView2NavigationStartingEventArgs e)
+    {
+        var context = DataContext as FeedItemListContext;
+
+        if (string.IsNullOrWhiteSpace(context?.DisplayUrl)) return;
+
+        if (e.Uri.Equals(context.DisplayUrl)) return;
+
+        e.Cancel = true;
+
+        ProcessHelpers.OpenUrlInExternalBrowser(e.Uri);
+    }
+
+    private void RssContentWebView_OnCoreWebView2InitializationCompleted(object? sender,
+        CoreWebView2InitializationCompletedEventArgs e)
     {
         RssContentWebView.CoreWebView2.BasicAuthenticationRequested += (o, args) =>
         {
             if (DataContext is not FeedItemListContext context) return;
-            
+
             args.Response.UserName = context.DisplayBasicAuthUsername;
             args.Response.Password = context.DisplayBasicAuthPassword;
         };
