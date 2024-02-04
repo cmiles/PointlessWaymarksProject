325e166f99bff28691bfb5976dd18fe3ddd3a962
Charles Miles <charles@cmiles.info>
2024-02-04 16:02:10 -0700

    CMS: Fix a bad string format change

--- PointlessWaymarks.CmsData/ContentHtml/LineHtml/LineParts.cs
+++ PointlessWaymarks.CmsData/ContentHtml/LineHtml/LineParts.cs
@@ -105,12 +105,12 @@ public static class LineParts
         var divScriptGuidConnector = Guid.NewGuid();
 
         var tag = $"""
-                  <div id=\"LineElevationContainer-{divScriptGuidConnector}\" class=\"line-elevation-chart-container\">
-                   <canvas id=\"LineElevationChart-{divScriptGuidConnector}\" class=\"line-elevation-chart\"></canvas>
+                  <div id="LineElevationContainer-{divScriptGuidConnector}" class="line-elevation-chart-container">
+                   <canvas id="LineElevationChart-{divScriptGuidConnector}" class="line-elevation-chart"></canvas>
                   </div>
                   """;
         var script =
-            $"<script>lazyInit(document.querySelector(\"#LineElevationChart-{divScriptGuidConnector}\"), () => singleLineElevationChartInit(document.querySelector(\"#LineElevationChart-{divScriptGuidConnector}\"), \"{content.ContentId}\"))</script>";
+            $"<script>lazyInit(document.querySelector('#LineElevationChart-{divScriptGuidConnector}'), () => singleLineElevationChartInit(document.querySelector('#LineElevationChart-{divScriptGuidConnector}'), '{content.ContentId}'))</script>";
 
         return tag + script;
     }
