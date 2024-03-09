78e139b440cf49a55fa3f18bba1a778621b08d1f
Charles Miles <charles@cmiles.info>
2024-03-09 09:23:46 -0700

    CMS: Add MD5 for stream

--- PointlessWaymarks.CommonTools/FileAndFolderTools.cs
+++ PointlessWaymarks.CommonTools/FileAndFolderTools.cs
@@ -1,4 +1,4 @@
-﻿using System.Security.Cryptography;
+using System.Security.Cryptography;
 using System.Text.RegularExpressions;
 using Serilog;
 
@@ -17,6 +17,23 @@ public static class FileAndFolderTools
     public static string CalculateMD5(this FileInfo file)
     {
         return CalculateMD5(file.FullName);
+    }
+
+    /// <summary>
+    /// Calculates and returns the MD5 hash of a stream - the stream position is reset to 0 before and after the hash is calculated.
+    /// </summary>
+    /// <param name="stream"></param>
+    /// <returns></returns>
+    public static string CalculateMD5(Stream stream)
+    {
+        stream.Position = 0;
+
+        using var md5 = MD5.Create();
+        var hash = md5.ComputeHash(stream);
+
+        stream.Position = 0;
+
+        return BitConverter.ToString(hash).Replace("-", "").ToLowerInvariant();
     }
 
     /// <summary>
@@ -46,6 +63,61 @@ public static class FileAndFolderTools
         if (forName == null) return null;
 
         return forName.FullName.Length > 240 ? $"\\\\?\\{forName.FullName}" : forName.FullName;
+    }
+
+    /// <summary>
+    ///     Returns the human-readable file size for an arbitrary, 64-bit file size
+    ///     The default format is "0.### XB", e.g. "4.2 KB" or "1.434 GB" - from
+    ///     https://www.somacon.com/p576.php
+    /// </summary>
+    /// <param name="i"></param>
+    /// <returns></returns>
+    public static string GetBytesReadable(long i)
+    {
+        // Get absolute value
+        var absoluteI = i < 0 ? -i : i;
+        // Determine the suffix and readable value
+        string suffix;
+        double readable;
+        if (absoluteI >= 0x1000000000000000) // Exabyte
+        {
+            suffix = "EB";
+            readable = i >> 50;
+        }
+        else if (absoluteI >= 0x4000000000000) // Petabyte
+        {
+            suffix = "PB";
+            readable = i >> 40;
+        }
+        else if (absoluteI >= 0x10000000000) // Terabyte
+        {
+            suffix = "TB";
+            readable = i >> 30;
+        }
+        else if (absoluteI >= 0x40000000) // Gigabyte
+        {
+            suffix = "GB";
+            readable = i >> 20;
+        }
+        else if (absoluteI >= 0x100000) // Megabyte
+        {
+            suffix = "MB";
+            readable = i >> 10;
+        }
+        else if (absoluteI >= 0x400) // Kilobyte
+        {
+            suffix = "KB";
+            readable = i;
+        }
+        else
+        {
+            return i.ToString("0 B"); // Byte
+        }
+
+        // Divide by 1024 to get fractional value
+        readable = readable / 1024;
+        // Return formatted number with suffix
+        return readable.ToString("0.# ") + suffix;
     }
 
     public static string InvalidFileNameCharsRegexPattern()
@@ -187,60 +259,5 @@ public static class FileAndFolderTools
         return string.IsNullOrWhiteSpace(filenameWithoutExtensionToTransform)
             ? string.Empty
             : Regex.Replace(filenameWithoutExtensionToTransform, InvalidFileNameCharsRegexPattern(), string.Empty);
-    }
-
-    /// <summary>
-    /// Returns the human-readable file size for an arbitrary, 64-bit file size 
-    /// The default format is "0.### XB", e.g. "4.2 KB" or "1.434 GB" - from
-    /// https://www.somacon.com/p576.php
-    /// </summary>
-    /// <param name="i"></param>
-    /// <returns></returns>
-    public static string GetBytesReadable(long i)
-    {
-        // Get absolute value
-        long absoluteI = (i < 0 ? -i : i);
-        // Determine the suffix and readable value
-        string suffix;
-        double readable;
-        if (absoluteI >= 0x1000000000000000) // Exabyte
-        {
-            suffix = "EB";
-            readable = (i >> 50);
-        }
-        else if (absoluteI >= 0x4000000000000) // Petabyte
-        {
-            suffix = "PB";
-            readable = (i >> 40);
-        }
-        else if (absoluteI >= 0x10000000000) // Terabyte
-        {
-            suffix = "TB";
-            readable = (i >> 30);
-        }
-        else if (absoluteI >= 0x40000000) // Gigabyte
-        {
-            suffix = "GB";
-            readable = (i >> 20);
-        }
-        else if (absoluteI >= 0x100000) // Megabyte
-        {
-            suffix = "MB";
-            readable = (i >> 10);
-        }
-        else if (absoluteI >= 0x400) // Kilobyte
-        {
-            suffix = "KB";
-            readable = i;
-        }
-        else
-        {
-            return i.ToString("0 B"); // Byte
-        }
-
-        // Divide by 1024 to get fractional value
-        readable = (readable / 1024);
-        // Return formatted number with suffix
-        return readable.ToString("0.# ") + suffix;
     }
 }
